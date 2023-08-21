b439faa6ddfd58c740a5a5bc559d7f78f4c00217
Lizzy Mills <lizzyhmills@gmail.com>
2023-08-21 16:11:58 +0100

    Added validations and associations to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  validates :bio, presence: true, length: { minimum: 5 }
+  has_many :yachts, dependent: :destroy
+  has_many :bookings, dependent: :destroy
 end
