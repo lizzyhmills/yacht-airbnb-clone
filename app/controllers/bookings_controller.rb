c0be2738dfad1e4294210f796721ccfbfc5ea445
Tom Fallis <tomfallis@icloud.com>
2023-08-22 14:14:08 +0100

    creating a new booking page for a specific user and yacht

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,5 +1,37 @@
 class BookingsController < ApplicationController
+  before_action :set_yacht, only: %i[new create]
+  before_action :set_user, only: %i[new create]
   def index
     @bookings = Booking.all
+  end
+
+  def new
+
+    @booking = Booking.new
+  end
+
+  def create
+    @booking = Booking.new(booking_params)
+    @booking.yacht = @yacht
+    @booking.user = @user
+    if @booking.save
+      redirect_to mybookings_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def set_yacht
+    @yacht = Yacht.find(params[:yacht_id])
+  end
+
+  def set_user
+    @user = current_user
+  end
+
+  def booking_params
+    params.require(:booking).permit(:check_in, :check_out, :guests)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,9 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
   get 'mybookings', to: "users#bookings"
-  resources :yachts
+  resources :yachts do
+    resources :bookings
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
