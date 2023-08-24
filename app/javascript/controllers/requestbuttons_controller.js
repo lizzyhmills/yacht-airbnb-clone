d647ea93a54e03779a7e5babfe8edd9b52c023a1
Nicoleta Grubi <msnicoleta@outlook.com>
2023-08-24 16:47:16 +0100

    buttons beautified

--- app/javascript/controllers/requestbuttons_controller.js
+++ app/javascript/controllers/requestbuttons_controller.js
@@ -2,17 +2,17 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="requestbuttons"
 export default class extends Controller {
-static targets = ["acceptButton", "rejectButton", "requestText"]
+  static targets = ["acceptButton", "rejectButton", "requestText"]
   // connect() {
   //   console.log("hello world")
   // }
 
-  disable(event){
+  disable(event) {
     event.preventDefault()
     console.log(this.acceptButtonTarget)
     if (event.target === this.acceptButtonTarget) {
-        this.requestTextTarget.innerHTML = "Request: accepted";
-      } else (event.target === this.rejectButtonTarget) {
+      this.requestTextTarget.innerHTML = "Request: accepted";
+    } else (event.target === this.rejectButtonTarget); {
       this.requestTextTarget.innerHTML = "Request: rejected";
     }
     this.acceptButtonTarget.classList.add("d-none");
